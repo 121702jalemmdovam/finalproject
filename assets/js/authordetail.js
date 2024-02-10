5d82406eebd6812ce2d8663a16298f82dab87cd6
jaleislam <146056487+121702jalemmdovam@users.noreply.github.com>
2024-02-11 01:20:32 +0400

    blog 50%

--- assets/js/authordetail.js
+++ assets/js/authordetail.js
@@ -16,7 +16,7 @@ function getproduct2(){
         <h5><strong>Name:</strong> ${item.name}</h5>
         <p class="title"><strong>Number of Books:</strong> ${item.title}</p>
         <p class="description"><strong>Description:</strong> ${item.description}</p>
-        <a href="/author.html"><button>Come Back</button></a>
+        <a href="/author.html"><button><i class="fa-solid fa-arrow-left"></i>  Come Back</button></a>
         </div>
         </div>
         </div>
@@ -25,4 +25,47 @@ function getproduct2(){
     })
 }
 getproduct2()
+
+
+const authorsdetailsitem=document.getElementById('authorsdetailsitem')
+let limit=8
+ let page=1
+function getproduct3(){
+    authorsdetailsitem.innerHTML=''
+    axios.get(`https://65680f199927836bd97406d3.mockapi.io/username/basket?page=${page}&limit=${limit}`)
+    .then(res=>{
+    products=res.data
+    products.map(item=>{
+        const box= document.createElement('div')
+        box.className='boxDiv col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12'
+        box.innerHTML=`
+        <div class="proDiv">
+        <img src="${item.image}" alt="">
+        <div class="textDiv">
+        <h5>${item.name}</h5>
+        <p>${item.title}</p>
+        </div>
+        <div class="btnDiv">
+        <a href="./authordetail.html"><button onclick="authordetail3(${item.id})">Read More <i class="fa-solid fa-arrow-right"></i></button></a>
+        </div>
+        </div>
+        `
+        authorsdetailsitem.appendChild(box);
+    }); 
+    page++
+    })
+}
+getproduct3()
+
+function authordetail3(id) {
+    // Yeni ürünü sepete eklemek için önceki verileri temizle
+    localStorage.removeItem('author');
+
+    // Yeni ürünü sepete ekle
+    const author = [products.find(item => item.id == id)];
+    
+    // Güncellenmiş sepet verilerini localStorage'a kaydet
+    localStorage.setItem('author', JSON.stringify(author));
+}
+
 
